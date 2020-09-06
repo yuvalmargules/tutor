a5c8c22626902c67aa299e353353eaae6f15e387
Moshenagar <mosesnagar@gmail.com>
2020-09-06 16:20:53 +0300

    Add flask-script and flask-migrate to handle migrations

--- run.py
+++ run.py
@@ -1,13 +1,15 @@
 from tutor import app, db
-from tutor.models import model
-
-
-db.create_all()
-
-
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 from tutor.routes import home
 from tutor.routes import model
 
 
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    manager.run()
+    # app.run(debug=True)
