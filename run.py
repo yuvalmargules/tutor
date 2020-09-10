213d1d1f3be341fc0268e9136fe5627e2be98fff
golan13 <55846420+golan13@users.noreply.github.com>
2020-09-10 09:42:13 +0300

    Revert "12_Initialize_DB"

--- run.py
+++ run.py
@@ -11,11 +11,5 @@ manager.add_command('db', MigrateCommand)
 
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    app.run(debug=True)
-
-db.create_all()
-=======
     manager.run()
     # app.run(debug=True)
->>>>>>> 954fd4845105b46bba7652256e5eb32bc5f1648a
