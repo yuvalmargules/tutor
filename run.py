90929fb955ba66cdab5d88c3134c1b034c9cef73
Michal-Shamir <michalsham4@gmail.com>
2020-09-14 20:15:10 +0300

    importing course from run

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from tutor import app, db
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from tutor.routes import home
+from tutor.routes import home, course
 from tutor.routes import model
 
 
