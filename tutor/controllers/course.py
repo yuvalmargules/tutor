340bda57ec3e1818ea6626e211feaffb6e0a79dd
yuvalmargules <yuvalmargules@gmail.com>
2020-09-14 19:43:11 +0300

    Adding to controller/course.py 2 functions- show courses on courses page(using dummy data), and adding course method.

--- tutor/controllers/course.py
+++ tutor/controllers/course.py
@@ -1,4 +1,13 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
+from .forms import AddCourse
+
+
+def addCourse():
+    form = AddCourse()
+    if form.validate_on_submit():
+        return redirect(url_for('courses_route'))
+    return render_template('addcourse.html', form=form)
+
 
 
 # TODO: need to be handled by backend
@@ -9,6 +18,16 @@ def getCourseResources(id):
 # TODO: need to be handled by backend
 def getCourseName(id):
     pass
+
+
+def showCourses():
+    # get courses from data base into a list
+    courses = [
+        {'id': 1, 'title': 'Course 1' },
+        {'id': 2, 'title': 'Course 2' },
+        {'id': 3, 'title': 'Course 3' }
+    ]
+    return render_template('courses.html', courses=courses)
 
 
 def showCoursePage(id):
