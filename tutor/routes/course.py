c32609a375bc70a9b620567f0d241915caf21c1e
yuvalmargules <yuvalmargules@gmail.com>
2020-09-16 20:29:18 +0300

    route home.py was split as adviced.

--- tutor/routes/course.py
+++ tutor/routes/course.py
@@ -2,6 +2,14 @@ from .. import app
 from ..controllers import course
 
 
+@app.route("/courses", methods=['GET', 'POST'])
+def courses_route():
+    return course.showCourses()
+
+@app.route("/addcourse", methods=['GET', 'POST'])
+def addCourse_route():
+    return course.addCourse()
+
 @app.route('/course/<id>')
 def get_course_info_route(id):
     return course.showCoursePage(id)
