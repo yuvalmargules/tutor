3ff4af24314745e508439b7791574272bd51330a
Golan Cohen <golan13@gmail.com>
2020-09-20 13:31:36 +0300

    Added ability to register user

--- tutor/controllers/register.py
+++ tutor/controllers/register.py
@@ -1,9 +1,15 @@
 from flask import render_template, redirect, url_for
 from .forms import RegistrationForm
+from ..models.users import Users
+from .. import db, bcrypt
 
 
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        return redirect(url_for('index_route'))
+        hashed_pass = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = Users(username=form.username.data, email=form.email.data, password=hashed_pass)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login_route'))
     return render_template('register.html', form=form)
---
+++ tutor/models/users.py
@@ -0,0 +1,12 @@
+from .. import db
+
+class Users(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return "'id': {0}, 'username': {1}, 'email': {2}".format(self.id, self.username, self.email)
