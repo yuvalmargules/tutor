305b362432a79cf9a78b32c74d15a6206b84beec
Golan Cohen <golan13@gmail.com>
2020-09-24 13:43:26 +0300

    Added user login functionality and test

--- tutor/__init__.py
+++ tutor/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from tutor.config import DevelopmentConfig
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
