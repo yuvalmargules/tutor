6d008c3fd003f311c03f221a0ec145ea46327ee8
Golan Cohen <golan13@gmail.com>
2020-09-24 11:29:55 +0300

    Modified register user test

--- tests/test_register.py
+++ tests/test_register.py
@@ -1,17 +1,24 @@
-from tutor import app, db
+from tutor import app, db, bcrypt
 from tutor.routes import home, course
 from tutor.models.users import Users
 
 
-def test_add_new_user():
-    c = app.test_client()
-    assert Users.query.all() == []
-    user = {
-        'username': 'test',
-        'email': 'test@email.com',
-        'password': '123',
-        'confirm_password': '123',
-        'submit': 'Sign Up'
-    }
-    c.post('/register', data=user, follow_redirects=True)
-    assert Users.query.first().username == 'test_user'
+def test_register_user():
+    app.config['WTF_CSRF_ENABLED'] = False
+    app.config['TESTING'] = True
+    with app.test_client() as c:
+        assert Users.query.all() == []
+        user = {
+            'username': 'test',
+            'email': 'test@email.com',
+            'password': '123',
+            'confirm_password': '123',
+            'submit': 'Sign Up'
+        }
+        c.post('/register', data=user, follow_redirects=True)
+        test_user = Users.query.first()
+        assert test_user.username == 'test'
+        assert test_user.email == 'test@email.com'
+        assert True == bcrypt.check_password_hash(test_user.password, '123')
+    Users.query.delete()
+    db.session.commit()
