bc6a7d55b20eebf7f98e862fb1d3f0c78e7c6485
Moshe Nagar <mosesnagar@gmail.com>
2020-09-24 13:53:59 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,26 +8,39 @@ apt install -y python3.7 python3-pip python3.7-dev postgresql postgresql-contrib
 
 echo "installing dependencies"
 pip3 install pipenv
-export PIPENV_VENV_IN_PROJECT="enabled"
-export VIRTUALENV_ALWAYS_COPY=1
-cd /vagrant/ && pipenv sync --dev
 
+# Not needed on CI
+if [ ! "$CI" ] 
+then
+  export PIPENV_VENV_IN_PROJECT="enabled"
+  export VIRTUALENV_ALWAYS_COPY=1
+  cd /vagrant
+fi
+
+pipenv sync --dev
+
+# Need to start postgres service on CI
+if [ "$CI" ]
+then
+  sudo systemctl start postgresql
+fi
 
 echo "initializing new DB"
 sudo -u postgres createdb tutor
 sudo -u postgres psql -c "ALTER ROLE postgres WITH PASSWORD 'tutor';"
-
 
 echo "configure flask variables"
 export FLASK_APP=/vagrant/run.py
 export FLASK_ENV=development    
 
 echo "initializing DB and migrations"
-cd /vagrant
 pipenv run python3 run.py db init
 pipenv run python3 run.py db migrate
 pipenv run python3 run.py db upgrade
 
-
-echo "run tutor app"
-nohup pipenv run flask run -h 0.0.0.0 -p 5000
+# No need to run the app on CI
+if [ ! "$CI" ] 
+then
+  echo "run tutor app"
+  nohup pipenv run flask run -h 0.0.0.0 -p 5000
+fi
