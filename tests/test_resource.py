0dca8e8575ed6564620c04a0877da7ba70cfcb22
Michal-Shamir <michalsham4@gmail.com>
2020-09-27 13:54:46 +0300

    change id

--- tests/test_resource.py
+++ tests/test_resource.py
@@ -8,8 +8,9 @@ def test_resource_create():
     app.config['WTF_CSRF_ENABLED'] = False
     app.config['TESTING'] = True
     with app.test_client() as c:
-        course_3 = Course(name='course_name')
-        db.session.add(course_3)
+        assert Resource.query.all() == []
+        course_1 = Course(name='course_name')
+        db.session.add(course_1)
         db.session.commit()
         resource = {
             'title': 'resource_name',
@@ -17,11 +18,11 @@ def test_resource_create():
             'link': 'www.google.com',
             'submit': 'Post'
         }
-        c.post('/resource/new?id=3', data=resource, follow_redirects=True)
+        c.post('/resource/new?id=1', data=resource, follow_redirects=True)
         test_resource = Resource.query.filter_by(title='resource_name').first()
         assert test_resource.title == 'resource_name'
         assert test_resource.content == 'content'
         assert test_resource.link == 'www.google.com'
     db.session.delete(test_resource)
-    db.session.delete(course_3)
+    db.session.delete(course_1)
     db.session.commit()
