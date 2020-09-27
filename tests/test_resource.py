a7e6e2ef5c66e8d0141368bb6035f0771e09b7d3
Michal-Shamir <michalsham4@gmail.com>
2020-09-27 13:08:22 +0300

    add creation of course to test

--- tests/test_resource.py
+++ tests/test_resource.py
@@ -1,5 +1,5 @@
 from tutor import app
-from tutor.routes import home, course, resources
+from tutor.routes import home, course, resources # noqa
 from tutor import db
 from tutor.models.course import Resource, Course
 
@@ -8,16 +8,20 @@ def test_resource_create():
     app.config['WTF_CSRF_ENABLED'] = False
     app.config['TESTING'] = True
     with app.test_client() as c:
+        course_3 = Course(name='course_name')
+        db.session.add(course_3)
+        db.session.commit()
         resource = {
             'title': 'resource_name',
             'content': 'content',
             'link': 'www.google.com',
             'submit': 'Post'
         }
-        c.post('/resource/new?id=1', data=resource, follow_redirects=True)
+        c.post('/resource/new?id=3', data=resource, follow_redirects=True)
         test_resource = Resource.query.filter_by(title='resource_name').first()
         assert test_resource.title == 'resource_name'
         assert test_resource.content == 'content'
         assert test_resource.link == 'www.google.com'
     db.session.delete(test_resource)
+    db.session.delete(course_3)
     db.session.commit()
